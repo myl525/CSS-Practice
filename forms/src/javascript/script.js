852458a12d16b21a4ff3a684a11f8d6fe70494cb
Mingyang Liu <castielliu525@gmail.com>
2022-06-29 21:28:03 -0400

    wrote 3 forms

--- forms/src/javascript/script.js
+++ forms/src/javascript/script.js
@@ -2,3 +2,13 @@ function adjust_textarea_4(ta) {
     ta.style.height = "20px";
     ta.style.height = (ta.scrollHeight) + "px";
 }
+
+function adjust_textarea_7(ta) {
+    ta.style.height = "26px";
+    ta.style.height = (ta.scrollHeight) + "px";
+}
+
+function adjust_textarea_8(ta) {
+    ta.style.height = "26px";
+    ta.style.height = (ta.scrollHeight) + "px";
+}
